ec46171075a05dcc84e6aabf10538095f317f9c8
AnuelAss <114248429+AnuelAss@users.noreply.github.com>
2022-09-23 13:28:39 -0300

    Add files via upload

---
+++ PM/html/js/codigo3.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function ($) {
+    function initialize() {
+      // var madryn = new google.maps.LatLng(-42.7732869,-65.0414546);
+      var mapOptions = {
+        zoom: 14,
+        center: {lat: -42.7732869, lng: -65.0414546}
+      }
+      var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    
+      // Carga de kml
+      $('#multiple').change(function(){
+        /$('.win-ring').css('');/
+        var check_KML = document.getElementById("multiple");
+    
+        if(check_KML.value=="General")
+        {
+         var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        }
+        else{var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+          var ctaLayer = new  google.maps.KmlLayer( url:Linea1.kml.value,map: map  );
+          //ctaLayer.setMap(map);
+          /$('.win-ring').hide();/} 
+    ;
